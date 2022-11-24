2b931a96e95cd750bdeac24f01d4f57b5a8a3260
Vinen Glasswala <vinen.g@cearsinfotech.com>
2022-11-24 10:55:12 +0530

    basics of js

---
+++ tuts.js
@@ -0,0 +1,34 @@
+// Functions
+function DoSomething(){
+
+}
+const DoSomething= ()=> {
+
+}
+let age =25;
+let name= age>25 ? "Vibha" : "Sona"
+console.log(name)
+
+// Ternary op
+// we will use it like this 
+return age>25 ?<div> Vibha</div> : <div> Ranee</div>;
+
+// Objects
+
+const person={
+        name :"Vibha",
+        age:24,
+        isMarried:false
+}
+
+const person2={...person, name:"Sona"}
+
+//Maps , filter
+
+const names=["vibha", "sona", "anurag"];
+names.map((names)=> {
+
+    console.log(names) 
+
+})
+
